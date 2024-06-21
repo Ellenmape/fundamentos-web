c9eeba1ea2910e3d93213e4338fe007f66cafc48
unknown <ellenmayarap@gmail.com>
2024-06-20 21:30:00 -0300

    incluindo JavaScript ao projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,94 @@ let nome = "Ellen" */
 
 //busca elementos pelo id
 
-let nome = document.querySelector("#nome")
-let email = document.querySelector("#email")
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const assunto = document.querySelector('#assunto');
+const mapa = document.querySelector('#mapa')
 
-console.log(nome);
-console.log(email);
+let nomeOk = false;
+let emailOk = false;
+let mensagemOk = false;
 
-//Função no JS == métodos no java
+//  console.log(nome);
+//  console.log(email)
+
+//  Funções no JS === Metodos Java
+
+// public void validaNome() {}
 
 function validaNome() {
- console.log(nome.value.length)
- if (nome.value.length >= 3){
- nome.style.borderColor = "green"
-}else {
- nome.style.borderColor = "red"
+  let txtNome = document.querySelector("#txtNome")
+
+  if (nome.value.length < 3) {
+    txtNome.innerHTML = "Nome muito curto"
+    txtNome.style.color = "red"
+    nomeOk = false
+  } else {
+    txtNome.innerHTML = 'Nome ok'
+    txtNome.style.color = "green"
+    nomeOk = true
+  }
 }
+
+function validaEmail() {
+  let txtEmail = document.querySelector('#txtEmail')
+  if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+    txtEmail.innerHTML = 'E-mail inválido'
+    txtEmail.style.color = "red"
+    emailOk = false
+  } else {
+    txtEmail.innerHTML = 'E-mail válido'
+    txtEmail.style.color = 'green'
+    emailOk = true
+  }
+}
+
+function validaEmail2() {
+  // criação do padrão de email, vai aceitar caracteres + @ + caracteres + . + 2 ou 3 caracteres pra finalizar
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  let txtEmail = document.querySelector('#txtEmail')
+
+  // o if será feito de uma forma diferente, verificando se o que a pessoa digitou condiz com o padrão do e-mail (match)
+  // caso de certo, e-mail válido, senão, e-mail inválido
+  if(email.value.match(regex)) {
+    txtEmail.innerHTML = 'E-mail válido'
+    txtEmail.style.color = 'green'
+    emailOk = true
+  } else {
+    txtEmail.innerHTML = 'E-mail inválido'
+    txtEmail.style.color = "red"
+    emailOk = false
+  }
+}
+
+function validaAssunto() {
+  let txtMsg = document.querySelector('#txtAssunto')
+  
+  if(assunto.value.length > 100) {
+    txtMsg.innerHTML = 'Assunto muito grande, digite no máximo 100 caracteres.'
+    txtMsg.style.color = '#d90429'
+    mensagemOk = false
+  } else {
+    txtMsg.innerHTML = assunto.value.length + '/100'
+    txtMsg.style.color = '#2b2d42'
+    mensagemOk = true
+  }
+}
+
+function enviarForm(){
+
+  if(nomeOk  && emailOk  && mensagemOk ) {
+    alert(nome.value + ', mensagem enviada com sucesso, aguarde um contato dos nosso pato chefe por e-mail.')
+  } else {
+    alert('Por favor, preencha todos os campos corretamente. Nossos patos-zumbis não são videntes e nem gostam de textão')
+  }
+}
+
+function mapaZoom() {
+  mapa.style.aspectRatio = '16/11'
+}
+
+function mapaNormal() {
+  mapa.style.aspectRatio = '16/9'
 }
